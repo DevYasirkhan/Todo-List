0a83164621b66680ce018b007a74e690b64d8b6e
Muhammad yasir <Yasirkhany11111@gmail.com>
2025-02-26 16:51:42 +0500

    Update App

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@
 const form = document.querySelector('.crud-form');
 const inputName = document.querySelector('.input-name');
 const inputDescription = document.querySelector('.input-description');
+const btnSubmit = document.querySelector('.btn-submit');
 const itemsList = document.querySelector('.items-list');
 
 //////////////////////////////////////////
 // Api Practice
-// let API_URL = 'https://crudcrud.com/api/';
-// let ID = '508cfe454cb4485098ca7b5bb8e8b41d';
+let API_URL = 'https://crudcrud.com/api/dd2a7a233af04138ba488a750487f774/items';
 
 document.addEventListener('DOMContentLoaded', fetchItems);
+
+let isEdit = false;
+let itemId;
 
 // Handle form submission
 form.addEventListener('submit', function (e) {
@@ -21,21 +24,26 @@ form.addEventListener('submit', function (e) {
   const name = inputName.value.trim();
   const description = inputDescription.value.trim();
 
-  createItem({ name, description });
+  if (!isEdit) {
+    createItem({ name, description });
+  } else {
+    updateItem(itemId, { name, description });
+    btnSubmit.textContent = 'Add Item';
+    isEdit = false;
+  }
   inputName.value = inputDescription.value = '';
 });
 
 // Create a new item
 async function createItem(data) {
   try {
-    await fetch(
-      'https://crudcrud.com/api/508cfe454cb4485098ca7b5bb8e8b41d/items',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      }
-    );
+    await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
     fetchItems();
   } catch (error) {
@@ -47,40 +55,38 @@ async function createItem(data) {
 async function fetchItems() {
   itemsList.innerHTML = '';
   try {
-    const response = await fetch(
-      'https://crudcrud.com/api/508cfe454cb4485098ca7b5bb8e8b41d/items'
-    );
+    const response = await fetch(API_URL);
     const items = await response.json();
-    console.log(response);
-    console.log(items);
 
     if (!response.ok) throw new Error('Failed to fetch items');
 
-    items.forEach(item => {
-      const html = `
-        <li class="item" data-id="${item._id}">
-            <span>${item.name} - ${item.description}</span>
-            <div>
-            <button class="btn-edit">Edit</button>
-            <button class="btn-delete">Delete</button>
-            </div>
-        </li>`;
-      itemsList.insertAdjacentHTML('afterbegin', html);
-    });
+    RenderItem(items);
   } catch (error) {
     console.error('Error Fetching items', error);
   }
 }
 
+// Render items
+function RenderItem(items) {
+  items.forEach(item => {
+    const html = `
+      <li class="item" data-id="${item._id}">
+          <span>${item.name} - ${item.description}</span>
+          <div>
+          <button class="btn-edit">Edit</button>
+          <button class="btn-delete">Delete</button>
+          </div>
+      </li>`;
+    itemsList.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
 // Delete items
 async function deleteItem(id) {
   try {
-    await fetch(
-      `https://crudcrud.com/api/508cfe454cb4485098ca7b5bb8e8b41d/items/${id}`,
-      {
-        method: 'DELETE',
-      }
-    );
+    await fetch(`${API_URL}/${id}`, {
+      method: 'DELETE',
+    });
     fetchItems();
   } catch (error) {
     console.error('Error deleting item', error);
@@ -90,14 +96,12 @@ async function deleteItem(id) {
 // Update items
 async function updateItem(id, updateData) {
   try {
-    await fetch(
-      `https://crudcrud.com/api/508cfe454cb4485098ca7b5bb8e8b41d/items/${id}`,
-      {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updateData),
-      }
-    );
+    await fetch(`${API_URL}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updateData),
+    });
+    fetchItems();
   } catch (error) {
     console.log('Error updating item', error);
   }
@@ -113,14 +117,14 @@ itemsList.addEventListener('click', function (e) {
 
   if (e.target.classList.contains('btn-edit')) {
     const itemElement = e.target.closest('.item');
-    const itemId = itemElement.dataset.id;
+    itemId = itemElement.dataset.id;
 
     const oldText = itemElement.querySelector('span').textContent;
     const [oldName, oldDescription] = oldText.split(' - ');
-
-    const newName = prompt('Enter new name', oldName);
-    const newDescription = prompt('Enter new description', oldDescription);
-
-    updateItem(itemId, { newName, newDescription });
+    inputName.value = oldName;
+    inputDescription.value = oldDescription;
+    btnSubmit.textContent = 'Edit Item';
+    isEdit = true;
+    inputName.focus();
   }
 });
